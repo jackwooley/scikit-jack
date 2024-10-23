0d66634347313d3e328f6d322f248d9428aa8eac
jackw <jack.wooley100@gmail.com>
2024-10-23 15:30:59 -0500

    added matrix transpose, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # scikit-jack
-Implementing ML algorithms from scratch (or close to it) to gain a greater familiarity of how the internals work. 
+Implementing ML algorithms from scratch (or close to it) to gain a greater familiarity of how the internals work with a focus on functionality and simple algorithms over performance.
--- jackutils/jackutils.py
+++ jackutils/jackutils.py
@@ -13,15 +13,7 @@ def matmul(A, B):
                         f'B: {len(B)} x {len(B[0])}\n')
     
     # to make indexing more easy, let's transpose B
-    # initialize empty matrix of correct dimensions so we can add stuff at the right indices
-    B_T = []
-    for i in range(len(B[0])):
-        B_T.append([0] * len(B))
-
-    # swapping the i,j positions to j,i
-    for i in range(len(B)):
-        for j in range(len(B[0])):
-            B_T[j][i] = B[i][j]
+    B_T = T(B)
 
     # initialize array of 0s to fill
     result = []
@@ -37,3 +29,30 @@ def matmul(A, B):
                 result[i][j] = element
 
     return result
+
+
+def T(A):
+    """Transpose a matrix A.
+    
+    Params:
+    :A -- either numpy array or list of lists, with each interior list having the same length.
+    Can handle scalars, but need to be formatted as list of list of length 1.
+    """
+
+    # check dimensions to make sure they're compatible
+    baseline = len(A[0])
+    lengths = [1 if len(i) == baseline else 0 for i in A]
+    if sum(lengths) != sum([1 for _ in A]):
+        raise ValueError('Are all the rows of your matrix the same shape?')
+    
+    # initialize empty matrix of correct dimensions so we can add stuff at the right indices
+    A_T = []
+    for i in range(len(A[0])):
+        A_T.append([0] * len(A))
+
+    # swapping the i,j positions to j,i
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            A_T[j][i] = A[i][j]
+    
+    return A_T
